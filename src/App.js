4c162bd5ce8ba61cd6a07bdabfd6afa5d0484ad8
tubaali13 <61675923+tubaali13@users.noreply.github.com>
2022-12-07 12:13:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Sandeep Branch
+          Learn React
         </a>
       </header>
     </div>
