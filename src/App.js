3952ffd08d7dd4b74656dec3d4502ed88ac99983
tubaali13 <tuba.13899@gmail.com>
2022-12-07 11:23:36 +0530

    changes in App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Creating React App
         </a>
       </header>
     </div>
